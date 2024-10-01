fa66df3481afc5b85ee27502d48749afea566c39
Th1nkK1D <witheep@gmail.com>
2024-10-01 15:13:18 +0700

    chore: add svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -18,5 +20,6 @@ export default defineConfig({
     react({
       experimentalReactChildren: true,
     }),
+    svelte(),
   ],
 });
