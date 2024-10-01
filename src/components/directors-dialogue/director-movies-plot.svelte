fafa362595a17c5ed7d3a4f3172be61edc4f690c
Th1nkK1D <witheep@gmail.com>
2024-10-01 18:09:22 +0700

    feat: plot director movies average wpm

---
+++ src/components/directors-dialogue/director-movies-plot.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import type { Movie } from "./directors-accordion.svelte";
+
+  export let movies: Movie[];
+  export let x: (value: number) => number;
+
+  let focusedMovie: Movie | null = null;
+</script>
+
+<div class="w-full h-full relative">
+  {#each movies.sort((a, z) => a.wordsPerMinute - z.wordsPerMinute) as movie (movie.id)}
+    <div
+      role="tooltip"
+      class="size-3 bg-neutral-100 absolute rounded-full -translate-y-1/2 -translate-x-1/2 top-1/2 border border-neutral-400"
+      style="left: {x(movie.wordsPerMinute)}%"
+      on:mouseover={() => (focusedMovie = movie)}
+      on:focus={() => (focusedMovie = movie)}
+      on:mouseleave={() => focusedMovie == movie && (focusedMovie = null)}
+    >
+      {#if focusedMovie?.id === movie.id}
+        <div
+          class="typo-b7 absolute left-1/2 bottom-3 whitespace-nowrap -translate-x-1/2"
+          transition:fade={{ duration: 100 }}
+        >
+          {movie.name} ({movie.year})
+        </div>
+      {/if}
+    </div>
+  {/each}
+</div>
--- src/components/directors-dialogue/directors-accordion.svelte
+++ src/components/directors-dialogue/directors-accordion.svelte
@@ -1,8 +1,28 @@
+<script lang="ts" context="module">
+  import directors from "../../../scripts/subtitles/out/directors.json";
+
+  export type Movie = (typeof directors)[number]["movies"][number];
+</script>
+
 <script lang="ts">
   import { createAccordion, melt } from "@melt-ui/svelte";
   import { slide } from "svelte/transition";
-  import directors from "../../../scripts/subtitles/out/directors.json";
+  import { scaleLinear } from "d3";
   import Chevron from "./chevron.svelte";
+  import DirectorMoviesPlot from "./director-movies-plot.svelte";
+
+  const X_AXIS_STEP = 25;
+
+  const maxWordsPerMinute = Math.max(
+    ...directors.flatMap(({ movies }) => movies.map((m) => m.wordsPerMinute)),
+  );
+
+  const upperBoundX = X_AXIS_STEP * Math.ceil(maxWordsPerMinute / X_AXIS_STEP);
+  const xAxes = new Array(upperBoundX / X_AXIS_STEP + 1)
+    .fill(null)
+    .map((_, i) => i * X_AXIS_STEP);
+
+  const x = scaleLinear([0, upperBoundX], [0, 100]);
 
   const {
     elements: { content, item, trigger, root },
@@ -10,30 +30,47 @@
   } = createAccordion();
 </script>
 
-<div class="flex w-full flex-col gap-2" {...$root}>
-  {#each directors as { name, nationality, movies } (name)}
-    {@const isOpened = $isSelected(name)}
-    <div use:melt={$item(name)} class="flex flex-col flex-1">
-      <button
-        use:melt={$trigger(name)}
-        class="flex flex-row flex-1 border border-neutral-100 rounded-sm"
+<div class="relative flex flex-col w-full">
+  <div class="flex flex-col gap-2 z-10 mt-14 mb-2" {...$root}>
+    {#each directors as { name, nationality, movies } (name)}
+      {@const isOpened = $isSelected(name)}
+      <div use:melt={$item(name)} class="flex flex-col flex-1">
+        <button
+          use:melt={$trigger(name)}
+          class="flex flex-row border border-neutral-500 rounded-sm"
+        >
+          <div class="flex flex-row items-start text-left w-64 py-3">
+            <Chevron {isOpened} />
+            <div class="flex-1 flex flex-col">
+              <h2 class="font-kondolar font-semibold typo-h8">{name}</h2>
+              <p>{nationality}</p>
+            </div>
+          </div>
+          <div class="h-full flex-1">
+            <DirectorMoviesPlot {movies} {x} />
+          </div>
+        </button>
+        {#if isOpened}
+          <div use:melt={$content(name)} transition:slide>
+            {JSON.stringify(movies)}
+          </div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+  <div class="absolute inset-0 left-64 flex typo-b6">
+    <p class="w-full text-center">Average Words Per Minute (WPM)</p>
+    {#each xAxes as axe}
+      <div
+        class="absolute top-8 bottom-0 w-[1px] bg-neutral-800"
+        style="left: {x(axe)}%;"
       >
-        <div class="py-3 pl-1">
-          <Chevron {isOpened} />
-        </div>
-        <div class="flex flex-col items-start text-left w-full max-w-64 py-3">
-          <h2 class="font-kondolar font-semibold typo-h8">{name}</h2>
-          <p>{nationality}</p>
-        </div>
-        <div class="h-full flex-1">
-          <div>TODO: scatter plot</div>
-        </div>
-      </button>
-      {#if isOpened}
-        <div use:melt={$content(name)} transition:slide>
-          {JSON.stringify(movies)}
-        </div>
-      {/if}
-    </div>
-  {/each}
+        <span
+          class="text-neutral-600 absolute {axe === upperBoundX
+            ? 'right-1'
+            : 'left-1'}">{axe}</span
+        >
+      </div>
+    {/each}
+  </div>
 </div>
