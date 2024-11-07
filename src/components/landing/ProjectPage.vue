300a2480c171720c925fa1535072d123a54086f3
Naanong (Key) <naanong@punchup.world>
2024-11-07 07:53:16 +0700

    feat: change design of landing page

---
+++ src/components/landing/ProjectPage.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import * as d3 from "d3";
+import Ticket from "./Ticket.vue";
+
+const projects = ref();
+const selectedProject = ref("");
+onMounted(async () => {
+  const rawProjects = await d3.csv("/projects.csv");
+  projects.value = rawProjects.slice(1).map((p) => {
+    p.hashtags = p.hashtag.split(" ");
+    return p;
+  });
+});
+
+const goToProject = (path) => {
+  selectedProject.value = path;
+  setTimeout(() => {
+    window.location.href = `/${path}`;
+  }, 450);
+};
+</script>
+
+<template>
+  <div>
+    <div v-for="project in projects" :key="project.id">
+      <div class="bg-black">
+        <div class="min-h-screen flex justify-center items-center">
+          <Ticket
+            :data="project"
+            :selectedProject="selectedProject"
+            @click="goToProject(project.path)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/landing/Ticket.vue
@@ -0,0 +1,138 @@
+<script setup>
+defineProps({
+  data: Object,
+  selectedProject: String,
+});
+</script>
+
+<template>
+  <div
+    class="bg-black overflow-hidden cursor-pointer transition-all duration-300 hover:rotate-[1deg]"
+  >
+    <div class="relative flex w-[1100px] bg-black">
+      <div
+        class="absolute z-10 top-0 left-0 -translate-x-1/2 -translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+      />
+      <div
+        class="absolute z-10 top-0 right-0 translate-x-1/2 -translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+      />
+      <div
+        class="absolute z-10 bottom-0 left-0 -translate-x-1/2 translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+      />
+      <div
+        class="absolute z-10 bottom-0 right-0 translate-x-1/2 translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+      />
+
+      <div
+        class="absolute top-0 left-0 -translate-x-1/2 z-20 flex flex-col justify-between h-full py-[60px]"
+      >
+        <div v-for="i in 10" :key="i">
+          <div class="rounded-full w-[35px] h-[35px] flex-none bg-black" />
+        </div>
+      </div>
+      <div
+        class="absolute top-0 right-0 translate-x-1/2 z-20 flex flex-col justify-between h-full py-[60px]"
+      >
+        <div v-for="i in 10" :key="i">
+          <div class="rounded-full w-[35px] h-[35px] flex-none bg-black" />
+        </div>
+      </div>
+
+      <div class="relative w-[75%] bg-[#FFF8D5] py-[30px] px-[80px]">
+        <div
+          class="absolute z-20 top-0 right-0 w-[10px] h-full border-r-[2px] border-dashed border-black"
+        ></div>
+
+        <div
+          class="absolute z-10 top-0 right-0 translate-x-1/2 -translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+        />
+        <div
+          class="absolute z-10 bottom-0 right-0 translate-x-1/2 translate-y-1/2 rounded-full w-[80px] h-[80px] flex-none bg-black"
+        />
+        <div class="w-full max-w-[650px]">
+          <img
+            class="w-full h-full object-cover"
+            :src="`/${data.devnick_en.toLowerCase()}.webp`"
+            alt=""
+          />
+          <div
+            class="bg-[#FFFC71] border-dashed border-t-[1px] border-b-[1px] border-black/50 absolute z-10 w-full left-0 right-0 h-[55px]"
+          >
+            <div
+              class="bg-[#FFFC71] border-dashed border-t-[1px] border-b-[1px] border-black/50 absolute top-1/2 -translate-y-1/2 right-0 translate-x-full w-full h-[55px]"
+            ></div>
+          </div>
+        </div>
+        <div class="relative z-20 py-[6px]">
+          <p class="typo-h5 font-bold leading-none">
+            {{ data.title }}
+          </p>
+        </div>
+        <p class="typo-b3 pt-2 h-[170px]">
+          {{ data.desc_en }}
+        </p>
+      </div>
+      <div
+        class="relative w-[25%] bg-white px-5 pt-[50px] pb-[65px] flex flex-col justify-between"
+      >
+        <div
+          :class="`bg-black absolute top-0 left-0 w-full transition duration-300 z-20 ${selectedProject === data.path && 'tear-ani'}`"
+        ></div>
+        <div>
+          <div
+            class="relative w-[100px] h-[100px] bg-white border-black border-[1px] rounded-full overflow-hidden"
+          >
+            <img
+              class="w-full h-full object-cover"
+              :src="`/${data.devnick_en.toLowerCase()}_profile.webp`"
+              alt=""
+            />
+            <div
+              class="star absolute top-0 right-0 translate-x-[70%] -translate-y-[70%]"
+            ></div>
+          </div>
+          <div class="pt-3">
+            <!-- <p class="typo-b6 opacity-50">developer</p> -->
+            <p class="typo-b3 font-bold leading-none">{{ data.devnick_en }}</p>
+            <p class="typo-b6">{{ data.devname_en }}</p>
+          </div>
+        </div>
+        <div class="">
+          <p
+            v-for="hashtag in data.hashtags"
+            :key="hashtag"
+            class="typo-b2 font-bold"
+          >
+            {{ hashtag }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.star {
+  width: 35px;
+  aspect-ratio: 1;
+  background: black;
+  clip-path: polygon(
+    50% 0,
+    calc(50% * (1 + sin(0.4turn))) calc(50% * (1 - cos(0.4turn))),
+    calc(50% * (1 - sin(0.2turn))) calc(50% * (1 - cos(0.2turn))),
+    calc(50% * (1 + sin(0.2turn))) calc(50% * (1 - cos(0.2turn))),
+    calc(50% * (1 - sin(0.4turn))) calc(50% * (1 - cos(0.4turn)))
+  );
+}
+.tear-ani {
+  animation: tear 0.5s ease-out forwards;
+}
+@keyframes tear {
+  from {
+    height: 0%;
+  }
+  to {
+    height: 100%;
+  }
+}
+</style>
